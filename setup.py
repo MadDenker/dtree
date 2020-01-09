9896a5bc24709b47013f301e571909767eb04ecb
Finco <Lucas.Finco@insight.com>
2020-01-09 13:30:09 -0500

    updated for dtree_pd

--- setup.py
+++ setup.py
@@ -13,26 +13,28 @@ class TestCommand(Command):
     def finalize_options(self):
         pass
     def run(self):
-        os.system('python dtree.py')
+        os.system('python dtree_pd.py')
 
 setup(
-    name='dtree',
-    version=dtree.__version__,
-    description='A simple pure-Python batch decision tree construction algorithm.',
-    author='Chris Spencer',
-    author_email='chrisspen@gmail.com',
-    url='https://github.com/chrisspen/dtree',
+    name='dtree_pd',
+    version=dtree_pd.__version__,
+    description='A Python decision tree using Pandas DataFrames.',
+    author='Lucas Finco',
+    author_email='lmfinco@uwalumni.com',
+    url='https://github.com/MadDenker/dtree_pd',
     license='LGPL',
-    py_modules=['dtree'],
-    #https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    py_modules=['dtree_pd'],
+    ###https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
+        "Intended Audience :: Data Scientists",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
@@ -40,7 +42,7 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     platforms=['OS Independent'],
-#    test_suite='dtree',
+#    test_suite='dtree_pd',
     cmdclass={
         'test': TestCommand,
     },
